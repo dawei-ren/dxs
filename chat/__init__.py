9c2bd21e1d6deff85f87bea644c520af1eef9004
David Ren <lovedavidren@outlook.com>
2020-10-13 23:54:54 +0800

    删除chat项目

--- chat/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-
-chat_app = Blueprint('chat', __name__)
-from . import urls
--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Api
-from chat.api.v1 import user, health
-
-from . import chat_app
-
-api = Api(chat_app)
-
-api.add_resource(user.User, '/api/v1/user')
-api.add_resource(health.Health, '/api/v1/health')
